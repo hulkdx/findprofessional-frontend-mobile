5296421698828d4b1e5ff610eca17d09ca391914
Mohammad Jafarzadeh Rezvan <hulktroll@gmail.com>
2021-02-20 20:36:21 +0200

    initial commit

---
+++ app/src/main/java/com/hulkdx/unknown/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.hulkdx.unknown
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.setContent
+import androidx.compose.ui.tooling.preview.Preview
+import com.hulkdx.unknown.ui.theme.AppTheme
+
+class MainActivity : AppCompatActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContent {
+      AppTheme {
+        // A surface container using the 'background' color from the theme
+        Surface(color = MaterialTheme.colors.background) {
+          Greeting("Android")
+        }
+      }
+    }
+  }
+}
+
+@Composable
+fun Greeting(name: String) {
+  Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+  AppTheme {
+    Greeting("Android")
+  }
+}
