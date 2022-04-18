1fc02d8905bae8c532ec7a7f410b380c857e7b55
hulkdx <hulktroll@gmail.com>
2022-04-18 14:17:40 +0300

    Android: Initial design of SignUpScreen

--- android/feature/authentication/src/main/java/com/hulkdx/findprofessional/feature/authentication/signup/SignUpScreen.kt
+++ android/feature/authentication/src/main/java/com/hulkdx/findprofessional/feature/authentication/signup/SignUpScreen.kt
@@ -1,20 +1,24 @@
 package com.hulkdx.findprofessional.feature.authentication.signup
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBarsPadding
-import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
-import androidx.compose.material.TextField
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.hulkdx.findprofessional.core.R
-import com.hulkdx.findprofessional.feature.authentication.login.LoginViewModel
 import com.hulkdx.findprofessional.feature.authentication.ui.EmailTextField
+import com.hulkdx.findprofessional.feature.authentication.ui.FilledButton
 import com.hulkdx.findprofessional.feature.authentication.ui.PasswordTextField
 import org.koin.androidx.compose.getViewModel
 
@@ -22,7 +26,8 @@ import org.koin.androidx.compose.getViewModel
 @Preview
 fun SignUpScreen(
     viewModel: SignUpViewModel = getViewModel(),
-) {
+
+    ) {
     var email by remember { mutableStateOf("") }
     var password by remember { mutableStateOf("") }
 
@@ -30,25 +35,36 @@ fun SignUpScreen(
         modifier = Modifier
             .fillMaxSize()
             .background(MaterialTheme.colors.onPrimary),
+        verticalArrangement = Arrangement.Center,
     ) {
         EmailTextField(
-            modifier = Modifier.statusBarsPadding(),
+            modifier = Modifier
+                .statusBarsPadding(),
             value = email,
-            onValueChanged = { email = it }
+            onValueChanged = { email = it },
         )
         PasswordTextField(
+            modifier = Modifier
+                .padding(top = 8.dp),
             value = password,
-            onValueChanged = { password = it }
+            onValueChanged = { password = it },
         )
-        SubmitButton(onClick = viewModel::onSubmitClicked)
+        SubmitButton(
+            modifier = Modifier
+                .padding(top = 16.dp),
+            onClick = viewModel::onSubmitClicked
+        )
     }
 }
 
 @Composable
-private fun SubmitButton(onClick: () -> Unit) {
-    Button(
+private fun SubmitButton(
+    modifier: Modifier = Modifier,
+    onClick: () -> Unit
+) {
+    FilledButton(
+        modifier = modifier,
+        text = stringResource(R.string.signUp),
         onClick = onClick,
-    ) {
-        Text(text = stringResource(R.string.signUp))
-    }
+    )
 }
