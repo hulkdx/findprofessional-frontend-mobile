868e5bf838d922ec74bcc9e80dc4071ae1bddb6c
hulkdx <hulktroll@gmail.com>
2023-01-21 00:32:18 +0200

    Checking builds

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,9 @@
 plugins {
     id("com.android.application").version(BuildDep.AGP_VERSION).apply(false)
+    id("com.android.library").version(BuildDep.AGP_VERSION).apply(false)
     id("org.jetbrains.kotlin.android").version(BuildDep.KOTLIN_VERSION).apply(false)
+    kotlin("multiplatform").version(BuildDep.KOTLIN_VERSION).apply(false)
+    id("com.rickclephas.kmp.nativecoroutines") version BuildDep.IOS_KMP_NATIVE_VERSION apply false
 }
 
 tasks.register("clean", Delete::class) {
