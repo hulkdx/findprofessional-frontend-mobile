a44fe6ac556016e587185a4912dbb16038396290
hulkdx <hulktroll@gmail.com>
2023-01-22 14:46:29 +0200

    Fixed tests

--- android/feature/authentication/src/test/java/com/hulkdx/findprofessional/feature/authentication/signup/SignUpViewModelTest.kt
+++ android/feature/authentication/src/test/java/com/hulkdx/findprofessional/feature/authentication/signup/SignUpViewModelTest.kt
@@ -1,19 +1,24 @@
 package com.hulkdx.findprofessional.feature.authentication.signup
 
+import com.hulkdx.findprofessional.common.feature.authentication.signup.SignUpApi
 import com.hulkdx.findprofessional.common.feature.authentication.signup.SignUpUseCase
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.extension.ExtendWith
+import org.mockito.Mock
 import org.mockito.junit.jupiter.MockitoExtension
 
 @ExtendWith(MockitoExtension::class)
 class SignUpViewModelTest {
     private lateinit var sut: SignUpViewModel
 
+    @Mock
+    private lateinit var api: SignUpApi
+
     @BeforeEach
     fun setUp() {
         sut = SignUpViewModel(
-            SignUpUseCase()
+            SignUpUseCase(api)
         )
     }
 }
