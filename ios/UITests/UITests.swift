08a51ed1b0e12279f4c1bf3b739370217af5d43b
hulkdx <hulktroll@gmail.com>
2023-01-23 17:05:20 +0200

    small change

--- ios/UITests/UITests.swift
+++ ios/UITests/UITests.swift
@@ -1,11 +1,3 @@
-//
-//  UITests.swift
-//  UITests
-//
-//  Created by Mohammad Jafarzadeh Rezvan on 1/22/23.
-//  Copyright © 2023 orgName. All rights reserved.
-//
-
 import XCTest
 
 class UITests: XCTestCase {
