e05e20f26804a241e88e75ef70b0ee7528f33f08
hulkdx <hulktroll@gmail.com>
2023-01-24 17:15:45 +0200

    Refactoring: BuildDep

--- buildSrc/src/main/java/BuildDep.kt
+++ buildSrc/src/main/java/BuildDep.kt
@@ -4,20 +4,27 @@
  * https://developer.android.com/jetpack/androidx/versions
  */
 object BuildDep {
+    // --------------------------------------------------------------------------------
+    // General
+    // NOTE: IOS_KMP_NATIVE_VERSION, KOTLIN_VERSION, COROUTINES_VERSION depends on each other
+    // --------------------------------------------------------------------------------
     const val MIN_SDK_VERSION = 24
     const val COMPILE_SDK_VERSION = 33
-
-    // https://github.com/rickclephas/KMP-NativeCoroutines
-    // NOTE:
-    // KOTLIN_VERSION     depends on this
-    // COROUTINES_VERSION depends on this
-    const val IOS_KMP_NATIVE_VERSION = "0.13.2-kotlin-1.8.0-RC"
-    // https://ktor.io/docs/getting-started-ktor-client-multiplatform-mobile.html#ktor-dependencies
-    const val KTOR_VERSION = "2.2.2"
 
     const val KOTLIN_VERSION = "1.8.0"
     // https://developer.android.com/studio/releases/gradle-plugin
     const val AGP_VERSION = "7.4.0"
+
+    // --------------------------------------------------------------------------------
+    // iOS
+    // --------------------------------------------------------------------------------
+
+    // https://github.com/rickclephas/KMP-NativeCoroutines
+    const val IOS_KMP_NATIVE_VERSION = "0.13.2-kotlin-1.8.0-RC"
+
+    // --------------------------------------------------------------------------------
+    // Android Main
+    // --------------------------------------------------------------------------------
 
     // https://developer.android.com/jetpack/androidx/releases/compose
     const val COMPOSE_VERSION = "1.4.0"
@@ -33,6 +40,12 @@ object BuildDep {
     const val KOIN_COMPOSE_VERSION = "3.4.1"
     // https://github.com/square/leakcanary
     const val LEAK_CANARY_VERSION = "2.8.1"
+    // https://ktor.io/docs/getting-started-ktor-client-multiplatform-mobile.html#ktor-dependencies
+    const val KTOR_VERSION = "2.2.2"
+
+    // --------------------------------------------------------------------------------
+    // Android Test
+    // --------------------------------------------------------------------------------
 
     // https://github.com/Kotlin/kotlinx.coroutines/tree/master/kotlinx-coroutines-test
     const val COROUTINES_TEST_VERSION = "1.5.2"
