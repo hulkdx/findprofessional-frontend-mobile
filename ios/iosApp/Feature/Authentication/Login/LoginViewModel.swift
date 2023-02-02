f0bac6c7a2651b77557b911f3d2e3c10df3b0eb2
hulkdx <hulktroll@gmail.com>
2023-02-02 21:30:37 +0200

    Refactor

--- ios/iosApp/Feature/Authentication/Login/LoginViewModel.swift
+++ ios/iosApp/Feature/Authentication/Login/LoginViewModel.swift
@@ -8,18 +8,11 @@ class LoginViewModel: ObservableObject {
     let useCase: LoginUseCase
     let viewModel: NavigationViewModel
 
-    @Published
-    var data = ""
-    
-//    var path = NavigationPath()
-    
     init(_ useCase: LoginUseCase,
          viewModel: NavigationViewModel
-//         , @Binding path: NavigationPath
     ) {
         self.useCase = useCase
         self.viewModel = viewModel
-//        self.path = path
     }
     
     func signInButtonClicked() {
