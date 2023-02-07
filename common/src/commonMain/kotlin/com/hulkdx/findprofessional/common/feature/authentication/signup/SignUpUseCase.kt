8ca96463489eb1173f627c89053a9a3d733b1941
hulkdx <hulktroll@gmail.com>
2023-02-07 20:23:34 +0200

    Refactoring

--- common/src/commonMain/kotlin/com/hulkdx/findprofessional/common/feature/authentication/signup/SignUpUseCase.kt
+++ common/src/commonMain/kotlin/com/hulkdx/findprofessional/common/feature/authentication/signup/SignUpUseCase.kt
@@ -4,33 +4,25 @@ import com.hulkdx.findprofessional.common.feature.authentication.signup.model.Re
 import com.hulkdx.findprofessional.common.navigation.NavigationScreen
 import com.hulkdx.findprofessional.common.navigation.Navigator
 import com.hulkdx.findprofessional.resources.MR
-import dev.icerock.moko.resources.desc.StringDesc
 import dev.icerock.moko.resources.desc.desc
 import io.ktor.client.plugins.*
 import io.ktor.http.*
-
 
 class SignUpUseCase(
     private val signUpApi: SignUpApi,
     private val navigator: Navigator,
 ) {
-    suspend fun register(request: RegisterRequest) = mapError {
+
+    suspend fun register(request: RegisterRequest) = try {
         signUpApi.register(request)
         navigator.navigate(NavigationScreen.Main)
+        null
+    } catch (e: Throwable) {
+        mapError(e)
     }
 
-    private inline fun mapError(block: () -> Unit): StringDesc? {
-        return try {
-            block()
-            null
-        } catch (e: Throwable) {
-            val error = if (e is ClientRequestException &&
-                e.response.status == HttpStatusCode.Conflict) {
-                MR.strings.emailExists
-            } else {
-                MR.strings.generalError
-            }
-            return error.desc()
-        }
+    private fun mapError(e: Throwable) = when ((e as? ClientRequestException)?.response?.status) {
+        HttpStatusCode.Conflict -> MR.strings.emailExists.desc()
+        else -> MR.strings.generalError.desc()
     }
 }
