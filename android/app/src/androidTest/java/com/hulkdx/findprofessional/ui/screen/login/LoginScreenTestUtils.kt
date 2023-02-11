7277431bcc99c908a7adb03fbfc7f87e373e29c6
hulkdx <hulktroll@gmail.com>
2023-02-11 15:54:37 +0200

    Test android ui tests

--- android/app/src/androidTest/java/com/hulkdx/findprofessional/ui/screen/login/LoginScreenTestUtils.kt
+++ android/app/src/androidTest/java/com/hulkdx/findprofessional/ui/screen/login/LoginScreenTestUtils.kt
@@ -29,7 +29,7 @@ class LoginDsl(
     }
 
     fun pressSignUpButton() {
-        rule.onNodeWithTextRes(MR.strings.signUp.resourceId, substring = true)
+        rule.onNodeWithTag("LoginScreen.SignUpButton")
             .performClick()
     }
 
--- android/feature/authentication/src/main/java/com/hulkdx/findprofessional/feature/authentication/login/LoginScreen.kt
+++ android/feature/authentication/src/main/java/com/hulkdx/findprofessional/feature/authentication/login/LoginScreen.kt
@@ -94,7 +94,7 @@ private fun SignUpButton(
     ) {
         TextButton(
             onClick = singleClick(onClick),
-            Modifier.testTag(stringResource(id = MR.strings.signUp.resourceId))
+            Modifier.testTag("LoginScreen.SignUpButton")
         ) {
             Text(
                 textAlign = TextAlign.Center,
