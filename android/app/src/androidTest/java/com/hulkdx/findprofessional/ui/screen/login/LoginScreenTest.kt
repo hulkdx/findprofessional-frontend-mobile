b794075225d195a89b47702910e0b678cf649182
hulkdx <hulktroll@gmail.com>
2023-02-12 12:42:26 +0200

    Fixed the issue with android uitests

--- android/app/src/androidTest/java/com/hulkdx/findprofessional/ui/screen/login/LoginScreenTest.kt
+++ android/app/src/androidTest/java/com/hulkdx/findprofessional/ui/screen/login/LoginScreenTest.kt
@@ -2,6 +2,7 @@ package com.hulkdx.findprofessional.ui.screen.login
 
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import com.hulkdx.findprofessional.MainActivity
+import com.hulkdx.findprofessional.utils.UiTestRule
 import org.junit.Rule
 import org.junit.Test
 
@@ -9,6 +10,9 @@ class LoginScreenTest {
 
     @get:Rule
     val composeRule = createAndroidComposeRule<MainActivity>()
+
+    @get:Rule
+    val rule = UiTestRule()
 
     @Test
     fun performSignUp() {
--- android/app/src/androidTest/java/com/hulkdx/findprofessional/ui/screen/signup/SignUpScreenTest.kt
+++ android/app/src/androidTest/java/com/hulkdx/findprofessional/ui/screen/signup/SignUpScreenTest.kt
@@ -3,6 +3,7 @@ package com.hulkdx.findprofessional.ui.screen.signup
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import com.hulkdx.findprofessional.MainActivity
 import com.hulkdx.findprofessional.common.config.api.InMemoryApi
+import com.hulkdx.findprofessional.utils.UiTestRule
 import org.junit.After
 import org.junit.Before
 import org.junit.Rule
@@ -12,6 +13,9 @@ class SignUpScreenTest {
 
     @get:Rule
     val composeRule = createAndroidComposeRule<MainActivity>()
+
+    @get:Rule
+    val rule = UiTestRule()
 
     @Before
     fun setUp() {
