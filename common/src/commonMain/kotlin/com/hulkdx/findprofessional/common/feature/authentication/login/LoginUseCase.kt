2efebb778bebafed03187d54fdc3c653e0bb756e
hulkdx <hulktroll@gmail.com>
2023-02-13 19:05:58 +0200

    General Error added

--- common/src/commonMain/kotlin/com/hulkdx/findprofessional/common/feature/authentication/login/LoginUseCase.kt
+++ common/src/commonMain/kotlin/com/hulkdx/findprofessional/common/feature/authentication/login/LoginUseCase.kt
@@ -3,7 +3,7 @@ package com.hulkdx.findprofessional.common.feature.authentication.login
 import com.hulkdx.findprofessional.common.feature.authentication.signup.model.AuthRequest
 import com.hulkdx.findprofessional.common.navigation.NavigationScreen
 import com.hulkdx.findprofessional.common.navigation.Navigator
-
+import com.hulkdx.findprofessional.common.utils.generalError
 
 class LoginUseCase(
     private val navigator: Navigator,
@@ -13,8 +13,11 @@ class LoginUseCase(
         navigator.navigate(NavigationScreen.SignUp)
     }
 
-    suspend fun onSignInClicked(request: AuthRequest) {
+    suspend fun onSignInClicked(request: AuthRequest) = try {
         api.login(request)
         navigator.navigate(NavigationScreen.Main)
+        null
+    } catch (e: Throwable) {
+        e.generalError()
     }
 }
--- common/src/commonMain/kotlin/com/hulkdx/findprofessional/common/feature/authentication/signup/SignUpUseCase.kt
+++ common/src/commonMain/kotlin/com/hulkdx/findprofessional/common/feature/authentication/signup/SignUpUseCase.kt
@@ -1,8 +1,11 @@
+@file:Suppress("MoveVariableDeclarationIntoWhen")
+
 package com.hulkdx.findprofessional.common.feature.authentication.signup
 
 import com.hulkdx.findprofessional.common.feature.authentication.signup.model.AuthRequest
 import com.hulkdx.findprofessional.common.navigation.NavigationScreen
 import com.hulkdx.findprofessional.common.navigation.Navigator
+import com.hulkdx.findprofessional.common.utils.generalError
 import com.hulkdx.findprofessional.resources.MR
 import dev.icerock.moko.resources.desc.desc
 import io.ktor.client.plugins.*
@@ -18,11 +21,11 @@ class SignUpUseCase(
         navigator.navigate(NavigationScreen.Main)
         null
     } catch (e: Throwable) {
-        mapError(e)
+        val statusCode = (e as? ClientRequestException)?.response?.status
+        when (statusCode) {
+            HttpStatusCode.Conflict -> MR.strings.emailExists.desc()
+            else -> e.generalError()
+        }
     }
 
-    private fun mapError(e: Throwable) = when ((e as? ClientRequestException)?.response?.status) {
-        HttpStatusCode.Conflict -> MR.strings.emailExists.desc()
-        else -> MR.strings.generalError.desc()
-    }
 }
