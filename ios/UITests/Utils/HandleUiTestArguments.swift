541cbfdb0168a9c59c8db14e937d3f84dd34f174
hulkdx <hulktroll@gmail.com>
2023-02-16 23:11:19 +0200

    UiTests add user handling

---
+++ ios/UITests/Utils/HandleUiTestArguments.swift
@@ -0,0 +1,18 @@
+import Foundation
+import shared
+
+#if DEBUG
+func handleUiTestArguments() {
+    ProcessInfo.processInfo.arguments.forEach { arg in
+        if arg == ArgKt.IN_MEMORY_API {
+            InMemoryApi.shared.loadKoinModules()
+        }
+        if arg.starts(with: ArgKt.IN_MEMORY_API_USER) {
+            let authRequestStr = arg.replacingOccurrences(of: ArgKt.IN_MEMORY_API_USER, with: "")
+            let authRequest = SerializableHelper().decodeAuthRequest(value: authRequestStr)
+            
+            InMemoryApi.shared.user = authRequest
+        }
+    }
+}
+#endif
