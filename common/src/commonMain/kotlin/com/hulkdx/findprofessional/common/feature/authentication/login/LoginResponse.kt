9233637d2bc8e76d3f1d2932f50fcd95f27cb1e5
hulkdx <hulktroll@gmail.com>
2023-02-19 17:45:53 +0200

    Fixed small bugs

--- common/src/commonMain/kotlin/com/hulkdx/findprofessional/common/feature/authentication/login/LoginResponse.kt
+++ common/src/commonMain/kotlin/com/hulkdx/findprofessional/common/feature/authentication/login/LoginResponse.kt
@@ -1,5 +1,8 @@
 package com.hulkdx.findprofessional.common.feature.authentication.login
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class LoginResponse(
     val accessToken: String,
     val refreshToken: String,
--- common/src/commonMain/kotlin/com/hulkdx/findprofessional/common/utils/ThrowableExt.kt
+++ common/src/commonMain/kotlin/com/hulkdx/findprofessional/common/utils/ThrowableExt.kt
@@ -4,10 +4,15 @@ import com.hulkdx.findprofessional.common.config.getPlatformSpecific
 import com.hulkdx.findprofessional.resources.MR
 import dev.icerock.moko.resources.desc.StringDesc
 import dev.icerock.moko.resources.desc.desc
+import io.ktor.client.plugins.*
 
 fun Throwable.generalError(): StringDesc {
     return if (getPlatformSpecific().isDebug()) {
-        stackTraceToString().desc()
+        if (this is ClientRequestException) {
+            message.desc()
+        } else {
+            stackTraceToString().desc()
+        }
     } else {
         MR.strings.generalError.desc()
     }
