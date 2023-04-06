39b0eb061dcaa86d92eac23b25fd92bc7dce33a4
hulkdx <hulktroll@gmail.com>
2023-04-06 15:55:19 +0300

    Completed LogoutUseCase

--- common/src/commonMain/kotlin/com/hulkdx/findprofessional/common/feature/authentication/logout/LogoutUseCase.kt
+++ common/src/commonMain/kotlin/com/hulkdx/findprofessional/common/feature/authentication/logout/LogoutUseCase.kt
@@ -2,6 +2,7 @@ package com.hulkdx.findprofessional.common.feature.authentication.logout
 
 import com.hulkdx.findprofessional.common.config.storage.AccessTokenStorage
 import com.hulkdx.findprofessional.common.config.storage.RefreshTokenStorage
+import com.hulkdx.findprofessional.common.navigation.NavigationScreen
 import com.hulkdx.findprofessional.common.navigation.Navigator
 
 interface LogoutUseCase {
@@ -14,5 +15,12 @@ class LogoutUseCaseImpl(
     private val refreshTokenStorage: RefreshTokenStorage,
 ): LogoutUseCase {
     override suspend fun logout() {
+        val isAlreadyLoggedOut = accessTokenStorage.get() == null && refreshTokenStorage.get() == null
+        if (isAlreadyLoggedOut) {
+            return
+        }
+        accessTokenStorage.remove()
+        refreshTokenStorage.remove()
+        navigator.navigate(NavigationScreen.Login)
     }
 }
