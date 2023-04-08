e2ed1931dc686563f62ba4834ea033e6c8dda65e
hulkdx <hulktroll@gmail.com>
2023-04-08 20:41:00 +0300

    Basic setup of HomeScreen

--- android/feature/home/src/main/kotlin/com/hulkdx/findprofessional/feature/home/HomeScreen.kt
+++ android/feature/home/src/main/kotlin/com/hulkdx/findprofessional/feature/home/HomeScreen.kt
@@ -1,22 +1,66 @@
 package com.hulkdx.findprofessional.feature.home
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.tooling.preview.Preview
 import com.hulkdx.findprofessional.core.theme.AppTheme
-
+import com.hulkdx.findprofessional.feature.authentication.ui.AppSnackBar
+import dev.icerock.moko.resources.compose.localized
+import org.koin.androidx.compose.getViewModel
 
 @Composable
-fun HomeScreen() {
-    Text(modifier = Modifier.testTag("HomeScreen"), text = "HomeScreen")
+fun HomeScreen(viewModel: HomeViewModel = getViewModel()) {
+    val error by viewModel.error.collectAsStateWithLifecycle()
+
+    HomeScreen(
+        error = error?.localized(),
+        onErrorDismissed = { viewModel.error.set(null) },
+    )
+}
+
+@Composable
+fun HomeScreen(
+    error: String?,
+    onErrorDismissed: () -> Unit,
+) {
+    Box(
+        modifier = Modifier
+            .testTag("HomeScreen")
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .verticalScroll(rememberScrollState())
+                .background(MaterialTheme.colorScheme.onPrimary),
+        ) {
+            Text("Testtting")
+        }
+        AppSnackBar(
+            modifier = Modifier.align(Alignment.BottomCenter),
+            message = error,
+            onDismiss = onErrorDismissed
+        )
+    }
 }
 
 @Preview
 @Composable
 private fun HomeScreenPreview() {
     AppTheme {
-        HomeScreen()
+        HomeScreen(
+            error = "",
+            onErrorDismissed = {}
+        )
     }
 }
