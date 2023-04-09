f90581fe8bd8f415805385c6e616d097225e3430
hulkdx <hulktroll@gmail.com>
2023-04-09 14:21:29 +0300

    Updated fonts

--- android/core/src/main/kotlin/com/hulkdx/findprofessional/core/theme/Typography.kt
+++ android/core/src/main/kotlin/com/hulkdx/findprofessional/core/theme/Typography.kt
@@ -10,19 +10,23 @@ import com.hulkdx.findprofessional.core.R
 // region Fonts
 
 val interFamily = FontFamily(
-    Font(R.font.inter_regular, FontWeight.Normal),
-    Font(R.font.inter_bold, FontWeight.Bold),
+    Font(R.font.inter_thin, FontWeight.Thin),
+    Font(R.font.inter_extralight, FontWeight.ExtraLight),
     Font(R.font.inter_light, FontWeight.Light),
+    Font(R.font.inter_regular, FontWeight.Normal),
     Font(R.font.inter_medium, FontWeight.Medium),
+    Font(R.font.inter_semibold, FontWeight.SemiBold),
+    Font(R.font.inter_bold, FontWeight.Bold),
+    Font(R.font.inter_extrabold, FontWeight.ExtraBold),
+    Font(R.font.inter_black, FontWeight.Black),
 )
 
 // endregion
 
 val h1 = TextStyle(
     fontFamily = interFamily,
-    fontWeight = FontWeight.Bold,
-    fontSize = 38.sp,
-    lineHeight = 42.sp,
+    fontWeight = FontWeight.ExtraBold,
+    fontSize = 30.sp,
 )
 
 val h3 = TextStyle(
