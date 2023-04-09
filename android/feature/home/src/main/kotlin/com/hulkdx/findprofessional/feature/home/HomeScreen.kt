59b915e7a4158094bed1d83d6db5599e5d220aa9
hulkdx <hulktroll@gmail.com>
2023-04-09 15:02:55 +0300

    Added text professional

--- android/feature/home/src/main/kotlin/com/hulkdx/findprofessional/feature/home/HomeScreen.kt
+++ android/feature/home/src/main/kotlin/com/hulkdx/findprofessional/feature/home/HomeScreen.kt
@@ -4,7 +4,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.navigationBarsPadding
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
@@ -14,11 +14,14 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.hulkdx.findprofessional.core.theme.AppTheme
+import com.hulkdx.findprofessional.core.theme.h1
 import com.hulkdx.findprofessional.feature.authentication.ui.AppSnackBar
+import com.hulkdx.findprofessional.resources.MR
 import dev.icerock.moko.resources.compose.localized
 import org.koin.androidx.compose.getViewModel
 
@@ -40,15 +43,19 @@ fun HomeScreen(
     Box(
         modifier = Modifier
             .background(MaterialTheme.colorScheme.onTertiary)
+            .statusBarsPadding()
             .testTag("HomeScreen")
     ) {
         Column(
             modifier = Modifier
                 .fillMaxSize()
                 .verticalScroll(rememberScrollState())
-                .background(MaterialTheme.colorScheme.onPrimary),
         ) {
-            Text("Testtting")
+            Text(
+                modifier = Modifier.padding(start = 20.dp),
+                text = stringResource(id = MR.strings.professionals.resourceId),
+                style = h1,
+            )
         }
         AppSnackBar(
             modifier = Modifier.align(Alignment.BottomCenter),
