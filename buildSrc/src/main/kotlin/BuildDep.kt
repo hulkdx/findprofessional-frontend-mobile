1c4f2896673c86491033153f422168947ea04d03
hulkdx <hulktroll@gmail.com>
2023-05-27 17:12:31 +0300

    Update lib versions

--- buildSrc/src/main/kotlin/BuildDep.kt
+++ buildSrc/src/main/kotlin/BuildDep.kt
@@ -15,7 +15,7 @@ object BuildDep {
     val JAVA_VERSION_SOURCE_COMPATIBILITY = JavaVersion.VERSION_17
     val JAVA_VERSION_TARGET_COMPATIBILITY = JAVA_VERSION_SOURCE_COMPATIBILITY
 
-    const val KOTLIN_VERSION = "1.8.10"
+    const val KOTLIN_VERSION = "1.8.21"
 
     // https://developer.android.com/studio/releases/gradle-plugin
     const val AGP_VERSION = "8.0.2"
@@ -35,25 +35,25 @@ object BuildDep {
     // --------------------------------------------------------------------------------
 
     // https://developer.android.com/jetpack/androidx/releases/compose
-    const val COMPOSE_VERSION = "1.4.4"
+    const val COMPOSE_VERSION = "1.4.7"
 
     // https://developer.android.com/jetpack/androidx/releases/compose
-    const val COMPOSE_BOM = "2023.04.00"
+    const val COMPOSE_BOM = "2023.05.01"
 
     // https://github.com/Kotlin/kotlinx.coroutines
-    const val COROUTINES_VERSION = "1.6.4"
+    const val COROUTINES_VERSION = "1.7.1"
 
     // https://github.com/InsertKoinIO/koin
     const val KOIN_VERSION = "3.4.0"
 
     // https://github.com/InsertKoinIO/koin
-    const val KOIN_COMPOSE_VERSION = "3.4.3"
+    const val KOIN_COMPOSE_VERSION = "3.4.4"
 
     // https://github.com/square/leakcanary
     const val LEAK_CANARY_VERSION = "2.10"
 
     // https://ktor.io/docs/getting-started-ktor-client-multiplatform-mobile.html#ktor-dependencies
-    const val KTOR_VERSION = "2.2.4"
+    const val KTOR_VERSION = "2.3.0"
 
     // https://github.com/google/accompanist
     const val ACCOMPANIST_VERSION = "0.30.1"
