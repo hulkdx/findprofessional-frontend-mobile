a7754026ca987fe04ddc930ff7a562aa6e8e9c86
hulkdx <hulktroll@gmail.com>
2023-06-03 19:21:16 +0300

    Adding SearchField

---
+++ android/core/src/main/kotlin/com/hulkdx/findprofessional/core/commonui/CUSearchField.kt
@@ -0,0 +1,70 @@
+package com.hulkdx.findprofessional.core.commonui
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun CUSearchField(
+    onSearch: (String) -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    var query by remember { mutableStateOf("") }
+
+    CUSearchField(
+        modifier = modifier,
+        query = query,
+        onQueryChange = { query = it },
+        onSearch = { onSearch(query) }
+    )
+}
+
+@Composable
+fun CUSearchField(
+    query: String,
+    onQueryChange: (String) -> Unit,
+    onSearch: KeyboardActionScope.() -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    TextField(
+        modifier = modifier,
+        value = query,
+        onValueChange = onQueryChange,
+        shape = RoundedCornerShape(10.dp),
+        colors = TextFieldDefaults.colors(
+            focusedContainerColor = MaterialTheme.colorScheme.onPrimary,
+            unfocusedContainerColor = MaterialTheme.colorScheme.onPrimary,
+            disabledContainerColor = MaterialTheme.colorScheme.onPrimary,
+            focusedIndicatorColor = Color.Transparent,
+            unfocusedIndicatorColor = Color.Transparent,
+            disabledIndicatorColor = Color.Transparent,
+        ),
+        placeholder = { Text(text = "Search") },
+        leadingIcon = { Icon(Icons.Filled.Search, contentDescription = "search") },
+        keyboardOptions = KeyboardOptions.Default.copy(imeAction = ImeAction.Search),
+        keyboardActions = KeyboardActions(onSearch = onSearch)
+    )
+
+}
+
+@Preview
+@Composable
+private fun CUTextField() {
+    CUSearchField(
+        query = "",
+        onQueryChange = {},
+        onSearch = {},
+    )
+}
