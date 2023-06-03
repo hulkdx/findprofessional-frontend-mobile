1c2e2ea4868c98d88c08c7175178f76bfc208b0b
hulkdx <hulktroll@gmail.com>
2023-06-03 15:59:29 +0300

    Developer screen done

--- common/src/commonMain/kotlin/com/hulkdx/findprofessional/common/config/storage/DeveloperStorage.kt
+++ common/src/commonMain/kotlin/com/hulkdx/findprofessional/common/config/storage/DeveloperStorage.kt
@@ -1,9 +1,11 @@
 package com.hulkdx.findprofessional.common.config.storage
 
 import kotlinx.coroutines.flow.Flow
+import org.koin.mp.KoinPlatformTools
 
 interface DeveloperStorage {
     fun getAsFlowBoolean(key: Key): Flow<Boolean?>
+    suspend fun getAsBoolean(key: Key): Boolean
     suspend fun setAsBoolean(key: Key, value: Boolean)
     suspend fun remove(key: Key)
 
@@ -14,4 +16,9 @@ interface DeveloperStorage {
             return "DeveloperStorage${super.toString()}"
         }
     }
+}
+
+suspend fun isMockData(): Boolean {
+    val d: DeveloperStorage = KoinPlatformTools.defaultContext().get().get()
+    return d.getAsBoolean(DeveloperStorage.Key.MockData)
 }
--- common/src/commonMain/kotlin/com/hulkdx/findprofessional/common/config/storage/datastore/DeveloperStorageDataStore.kt
+++ common/src/commonMain/kotlin/com/hulkdx/findprofessional/common/config/storage/datastore/DeveloperStorageDataStore.kt
@@ -5,13 +5,13 @@ import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.booleanPreferencesKey
 import com.hulkdx.findprofessional.common.config.storage.DeveloperStorage
 import com.hulkdx.findprofessional.common.config.storage.DeveloperStorage.Key
-import kotlinx.coroutines.flow.Flow
 
 
 class DeveloperStorageDataStore(
     private val dataStore: DataStore<Preferences>,
 ): DeveloperStorage {
     override fun getAsFlowBoolean(key: Key) = dataStore.getFlow(booleanPreferencesKey(key.toString()))
+    override suspend fun getAsBoolean(key: Key) = dataStore.getAsBoolean(key.toString()) ?: false
     override suspend fun setAsBoolean(key: Key, value: Boolean) = dataStore.setAsBoolean(key.toString(), value)
     override suspend fun remove(key: Key) = dataStore.removeBoolean(key.toString())
 }
