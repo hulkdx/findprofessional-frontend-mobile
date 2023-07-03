27fb97b8958a1259513afc659bbe5a658d3b430c
hulkdx <hulktroll@gmail.com>
2023-07-03 18:47:16 +0300

    Profile: Logotu added

--- android/feature/profile/src/main/kotlin/com/hulkdx/findprofessional/feature/profile/ProfileScreen.kt
+++ android/feature/profile/src/main/kotlin/com/hulkdx/findprofessional/feature/profile/ProfileScreen.kt
@@ -1,15 +1,18 @@
 package com.hulkdx.findprofessional.feature.profile
 
-import androidx.annotation.DrawableRes
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.systemBarsPadding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowRight
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -17,14 +20,15 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.platform.testTag
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.hulkdx.findprofessional.common.navigation.Navigator
-import com.hulkdx.findprofessional.core.R
 import com.hulkdx.findprofessional.core.commonui.CUSnackBar
 import com.hulkdx.findprofessional.core.theme.AppTheme
+import com.hulkdx.findprofessional.core.theme.h3
 import com.hulkdx.findprofessional.feature.navigation.AppNavigationBar
 import com.hulkdx.findprofessional.feature.navigation.StubNavigator
 import dev.icerock.moko.resources.compose.localized
@@ -37,6 +41,7 @@ fun ProfileScreen(viewModel: ProfileViewModel = getViewModel()) {
     val error by viewModel.error.collectAsStateWithLifecycle()
 
     ProfileScreen(
+        onLogoutClicked = viewModel::onLogoutClicked,
         navigator = koinInject(),
         error = error?.localized(),
         onErrorDismissed = { viewModel.error.set(null) },
@@ -45,6 +50,7 @@ fun ProfileScreen(viewModel: ProfileViewModel = getViewModel()) {
 
 @Composable
 private fun ProfileScreen(
+    onLogoutClicked: () -> Unit,
     navigator: Navigator,
     error: String?,
     onErrorDismissed: () -> Unit,
@@ -59,8 +65,8 @@ private fun ProfileScreen(
         Column {
             ProfileItem(
                 text = "Logout",
-                icon = R.drawable.ic_like,
-                onClick = {}
+                icon = Icons.Filled.KeyboardArrowRight,
+                onClick = onLogoutClicked,
             )
         }
         CUSnackBar(
@@ -75,21 +81,26 @@ private fun ProfileScreen(
 @Composable
 private fun ProfileItem(
     text: String,
-    @DrawableRes icon: Int,
+    icon: ImageVector,
     onClick: () -> Unit,
 ) {
     Row(
+        horizontalArrangement = Arrangement.SpaceBetween,
         modifier = Modifier
             .fillMaxWidth()
+            .padding(16.dp)
+            .clip(RoundedCornerShape(8.dp))
+            .background(MaterialTheme.colorScheme.onPrimary)
             .clickable(onClick = onClick)
+            .padding(16.dp),
     ) {
         Text(
-            modifier = Modifier.weight(1f),
+            modifier = Modifier,
             text = text,
+            style = h3,
         )
         Icon(
-            modifier = Modifier.size(24.dp),
-            painter = painterResource(icon),
+            imageVector = icon,
             contentDescription = "",
         )
     }
@@ -100,6 +111,7 @@ private fun ProfileItem(
 private fun ProfileScreenPreview() {
     AppTheme {
         ProfileScreen(
+            onLogoutClicked = {},
             navigator = StubNavigator(),
             error = "",
             onErrorDismissed = {},
