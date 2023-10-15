665461e7f7b06b65784b242436b8f42e8027ed01
hulkdx <hulktroll@gmail.com>
2023-10-15 17:55:29 +0300

    Change navigation animation of home,profile

--- android/feature/home/src/main/kotlin/com/hulkdx/findprofessional/feature/home/HomeNavigationScreen.kt
+++ android/feature/home/src/main/kotlin/com/hulkdx/findprofessional/feature/home/HomeNavigationScreen.kt
@@ -1,8 +1,8 @@
 package com.hulkdx.findprofessional.feature.home
 
+import com.hulkdx.findprofessional.feature.navigation.screen.BasicNavigationScreen
 import com.hulkdx.findprofessional.feature.navigation.screen.Content
-import com.hulkdx.findprofessional.feature.navigation.screen.SlideNavigationScreen
 
-class HomeNavigationScreen : SlideNavigationScreen() {
+class HomeNavigationScreen : BasicNavigationScreen() {
     override val content: Content = { HomeScreen() }
 }
--- android/feature/profile/src/main/kotlin/com/hulkdx/findprofessional/feature/profile/ProfileNavigationScreen.kt
+++ android/feature/profile/src/main/kotlin/com/hulkdx/findprofessional/feature/profile/ProfileNavigationScreen.kt
@@ -1,9 +1,9 @@
 package com.hulkdx.findprofessional.feature.profile
 
+import com.hulkdx.findprofessional.feature.navigation.screen.BasicNavigationScreen
 import com.hulkdx.findprofessional.feature.navigation.screen.Content
-import com.hulkdx.findprofessional.feature.navigation.screen.SlideNavigationScreen
 
 
-class ProfileNavigationScreen : SlideNavigationScreen() {
+class ProfileNavigationScreen : BasicNavigationScreen() {
     override val content: Content = { ProfileScreen() }
 }
