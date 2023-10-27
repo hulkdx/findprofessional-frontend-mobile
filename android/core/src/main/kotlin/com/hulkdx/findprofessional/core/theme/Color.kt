0d338a859f542afe390ba65add06aafe369a3659
hulkdx <hulktroll@gmail.com>
2023-10-27 21:49:55 +0300

    background color change

--- android/core/src/main/kotlin/com/hulkdx/findprofessional/core/theme/Color.kt
+++ android/core/src/main/kotlin/com/hulkdx/findprofessional/core/theme/Color.kt
@@ -9,7 +9,7 @@ private val Green = Color(0xFF0F6A2C)
 private val LightGreen = Color(0xFFD4E0D8)
 private val White = Color(0xFFFFFFFF)
 private val LightBlue = Color(0xFFE1EFFF)
-private val LightGrey = Color(0xFFF4F4F4)
+private val LightGrey = Color(0xFFF5F6F9)
 private val Gray = Color(0xFF8B8B8B)
 private val Yellow = Color(0xFFF6C548)
 
