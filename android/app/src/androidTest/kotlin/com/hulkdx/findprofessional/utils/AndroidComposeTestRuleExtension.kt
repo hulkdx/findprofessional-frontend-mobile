0a52c380bd3e1273f996343aa41b5e244211f71c
hulkdx <hulktroll@gmail.com>
2023-11-25 21:28:39 +0200

    Fix test

--- android/app/src/androidTest/kotlin/com/hulkdx/findprofessional/utils/AndroidComposeTestRuleExtension.kt
+++ android/app/src/androidTest/kotlin/com/hulkdx/findprofessional/utils/AndroidComposeTestRuleExtension.kt
@@ -1,5 +1,6 @@
 package com.hulkdx.findprofessional.utils
 
+import android.os.SystemClock
 import androidx.annotation.StringRes
 import androidx.compose.ui.test.SemanticsNodeInteraction
 import androidx.compose.ui.test.assertIsDisplayed
@@ -9,6 +10,8 @@ import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.onRoot
 import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.uiautomator.UiDevice
 import com.hulkdx.findprofessional.MainActivity
 
 typealias Rule = AndroidComposeTestRule<ActivityScenarioRule<MainActivity>, MainActivity>
@@ -41,13 +44,17 @@ fun Rule.assertNodeIsDisplayed(testTag: String) {
         .assertIsDisplayed()
 }
 
-fun Rule.pressBackButton() {
-    activityRule.scenario.onActivity { activity ->
-        activity.onBackPressedDispatcher.onBackPressed()
+// TODO: Move it to another class
+val device by lazy { UiDevice.getInstance(InstrumentationRegistry.getInstrumentation()) }
+
+fun Rule.pressBackButton(timeoutMillis: Long = 10_000) {
+    waitUntil(timeoutMillis) {
+        device.pressBack()
     }
 }
 
 fun Rule.assertAppIsClosed(timeoutMillis: Long = 10_000) {
+    SystemClock.sleep(1000)
     waitUntil(timeoutMillis) {
         runCatching { onRoot().assertDoesNotExist() }.isSuccess
     }
