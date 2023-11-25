32aa85b4ebd4a7e74e33b2f34369795f3e5e57e6
hulkdx <hulktroll@gmail.com>
2023-11-25 20:43:30 +0200

    Testing back press

--- android/app/src/androidTest/kotlin/com/hulkdx/findprofessional/utils/AndroidComposeTestRuleExtension.kt
+++ android/app/src/androidTest/kotlin/com/hulkdx/findprofessional/utils/AndroidComposeTestRuleExtension.kt
@@ -1,7 +1,6 @@
 package com.hulkdx.findprofessional.utils
 
 import androidx.annotation.StringRes
-import androidx.compose.ui.test.ComposeTimeoutException
 import androidx.compose.ui.test.SemanticsNodeInteraction
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.AndroidComposeTestRule
@@ -9,7 +8,6 @@ import androidx.compose.ui.test.onAllNodesWithTag
 import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.onRoot
-import androidx.test.espresso.Espresso
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import com.hulkdx.findprofessional.MainActivity
 
@@ -43,25 +41,14 @@ fun Rule.assertNodeIsDisplayed(testTag: String) {
         .assertIsDisplayed()
 }
 
-@Suppress("UnusedReceiverParameter")
 fun Rule.pressBackButton() {
-    // Minor: Find a way to do this with jetpack compose test
-    Espresso.pressBackUnconditionally()
+    activityRule.scenario.onActivity { activity ->
+        activity.onBackPressedDispatcher.onBackPressed()
+    }
 }
 
 fun Rule.assertAppIsClosed(timeoutMillis: Long = 10_000) {
-    val startTime = System.currentTimeMillis()
-    while (true) {
-        try {
-            onRoot().assertDoesNotExist()
-            break
-        } catch (e: AssertionError) {
-            Thread.sleep(100)
-            if (System.currentTimeMillis() - startTime > timeoutMillis) {
-                throw ComposeTimeoutException(
-                    "Condition still not satisfied after $timeoutMillis ms"
-                )
-            }
-        }
+    waitUntil(timeoutMillis) {
+        runCatching { onRoot().assertDoesNotExist() }.isSuccess
     }
 }
