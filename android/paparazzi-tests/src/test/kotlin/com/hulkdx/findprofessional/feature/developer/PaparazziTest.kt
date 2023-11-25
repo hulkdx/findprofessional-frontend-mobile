7d75f30b68ad2e024fc8e7a68b06452ab53b10cc
hulkdx <hulktroll@gmail.com>
2023-11-25 16:00:31 +0200

    SignupScreen paparazzi test added

--- android/paparazzi-tests/src/test/kotlin/com/hulkdx/findprofessional/feature/developer/PaparazziTest.kt
+++ android/paparazzi-tests/src/test/kotlin/com/hulkdx/findprofessional/feature/developer/PaparazziTest.kt
@@ -3,6 +3,7 @@ package com.hulkdx.findprofessional.feature.developer
 import app.cash.paparazzi.Paparazzi
 import com.hulkdx.findprofessional.core.theme.AppTheme
 import com.hulkdx.findprofessional.feature.authentication.login.LoginScreen
+import com.hulkdx.findprofessional.feature.authentication.signup.SignUpScreen
 import com.hulkdx.findprofessional.feature.authentication.splash.Splash
 import org.junit.Before
 import org.junit.Rule
@@ -46,4 +47,24 @@ class PaparazziTest {
         }
     }
 
+    @Test
+    fun `Sign paparazzi test`() {
+        paparazzi.snapshot {
+            AppTheme {
+                SignUpScreen(
+                    firstName = "",
+                    onFirstNameChanged = {},
+                    lastName = "",
+                    onLastNameChanged = {},
+                    email = "",
+                    onEmailChanged = {},
+                    password = "",
+                    onPasswordChanged = {},
+                    onSubmitClicked = {},
+                    error = "",
+                    onErrorDismissed = {}
+                )
+            }
+        }
+    }
 }
