a34a907d7c209e3d3a0564aa5a6048eb875268a7
hulkdx <hulktroll@gmail.com>
2023-12-27 14:39:04 +0200

    Added initial failing test

--- android/feature/home/src/test/kotlin/com/hulkdx/findprofessional/feature/home/detail/utils/AvailabilityDataTest.kt
+++ android/feature/home/src/test/kotlin/com/hulkdx/findprofessional/feature/home/detail/utils/AvailabilityDataTest.kt
@@ -1,13 +1,40 @@
 package com.hulkdx.findprofessional.feature.home.detail.utils
 
-import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import java.time.LocalDate
 
 
 class AvailabilityDataTest {
 
-    @BeforeAll
-    fun setup() {
+    @Test
+    fun isSelectedDayTest() {
+        // Arrange
+        val now = LocalDate.of(2023, 12, 27)
+        val selectedItemForThisMonth = listOf(
+            LocalDate.of(2023, 12, 27)
+        )
+
+        val availabilityData = createAvailabilityData(
+            now,
+            selectedItemForThisMonth,
+        )
+        // Act
+        val result = availabilityData.isSelectedDay(27)
+        // Assert
+        assertEquals(true, result)
     }
 
+    private fun createAvailabilityData(
+        now: LocalDate,
+        selectedItemForThisMonth: List<LocalDate>,
+    ) =
+        AvailabilityData(
+            calendarDateFormat = "",
+            firstDay = 0,
+            lengthOfMonth = 0,
+            now = now.toEpochDay(),
+            selectedItemForThisMonth = selectedItemForThisMonth
+        )
 
 }
