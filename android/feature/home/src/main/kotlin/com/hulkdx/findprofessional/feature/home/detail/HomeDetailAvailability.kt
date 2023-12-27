6c690f872345b93b40043894ec6dda151c4243bb
hulkdx <hulktroll@gmail.com>
2023-12-27 12:06:41 +0200

    initial design Availability calendar items

--- android/feature/home/src/main/kotlin/com/hulkdx/findprofessional/feature/home/detail/HomeDetailAvailability.kt
+++ android/feature/home/src/main/kotlin/com/hulkdx/findprofessional/feature/home/detail/HomeDetailAvailability.kt
@@ -4,12 +4,17 @@ package com.hulkdx.findprofessional.feature.home.detail
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListScope
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Divider
 import androidx.compose.material3.IconButton
@@ -19,6 +24,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
@@ -123,7 +130,7 @@ private fun CalendarPart(
 }
 
 @Composable
-private fun CalendarEachDate(
+private fun ColumnScope.CalendarEachDate(
     i: Int,
     j: Int,
     firstDay: Int,
@@ -131,12 +138,22 @@ private fun CalendarEachDate(
 ) {
     val z = (i + 1) + (j * 7) - firstDay
     if (z <= 0 || z > lastDay) {
-        Text(text = "")
+        Box(modifier = Modifier.aspectRatio(1F))
     } else {
-        Text(
-            modifier = Modifier,
-            text = z.toString()
-        )
+        Box(
+            modifier = Modifier
+                .aspectRatio(1F)
+                .padding(4.dp)
+                .drawBehind {
+                    drawCircle(Color.Green)
+                }
+        ) {
+            Text(
+                modifier = Modifier.align(Alignment.Center),
+                text = z.toString(),
+                color = Color.White
+            )
+        }
     }
 }
 
