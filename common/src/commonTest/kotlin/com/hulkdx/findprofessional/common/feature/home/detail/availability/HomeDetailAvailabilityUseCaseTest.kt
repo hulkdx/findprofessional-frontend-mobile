1540a0ce537fc6537e7a4ba29bf1b5ca73b9c636
hulkdx <hulktroll@gmail.com>
2023-12-28 16:13:43 +0200

    Small change

--- common/src/commonTest/kotlin/com/hulkdx/findprofessional/common/feature/home/detail/availability/HomeDetailAvailabilityUseCaseTest.kt
+++ common/src/commonTest/kotlin/com/hulkdx/findprofessional/common/feature/home/detail/availability/HomeDetailAvailabilityUseCaseTest.kt
@@ -2,15 +2,7 @@
 
 package com.hulkdx.findprofessional.common.feature.home.detail.availability
 
-import com.hulkdx.findprofessional.common.feature.home.model.Professional
-import com.hulkdx.findprofessional.common.feature.home.model.ProfessionalAvailability
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
 import kotlinx.datetime.LocalDate
-import kotlinx.datetime.toLocalDate
-import kotlinx.datetime.toLocalTime
 import kotlin.test.BeforeTest
 import kotlin.test.Test
 import kotlin.test.assertEquals
@@ -65,76 +57,4 @@ class HomeDetailAvailabilityUseCaseTest {
         // Assert
         assertEquals(expected, result)
     }
-
-    @Test
-    fun bla() = runBlocking {
-        val pro = MutableStateFlow(
-            professional(
-                listOf(
-                    ProfessionalAvailability(
-                        date = "2023-11-09".toLocalDate(),
-                        from = "08:00".toLocalTime(),
-                        to = "09:00".toLocalTime(),
-                    ),
-                    ProfessionalAvailability(
-                        date = "2023-11-10".toLocalDate(),
-                        from = "07:00".toLocalTime(),
-                        to = "08:00".toLocalTime(),
-                    ),
-                    ProfessionalAvailability(
-                        date = "2023-11-11".toLocalDate(),
-                        from = "09:00".toLocalTime(),
-                        to = "11:00".toLocalTime(),
-                    ),
-                    ProfessionalAvailability(
-                        date = "2023-11-12".toLocalDate(),
-                        from = "12:00".toLocalTime(),
-                        to = "15:00".toLocalTime(),
-                    ),
-                    ProfessionalAvailability(
-                        date = "2023-11-13".toLocalDate(),
-                        from = "20:00".toLocalTime(),
-                        to = "00:00".toLocalTime(),
-                    ),
-                )
-            )
-        )
-        val d = MutableStateFlow(LocalDate(2023, 10, 1))
-        launch {
-            delay(2000)
-            println("updating pro")
-            pro.value = professional(
-                listOf(
-                    ProfessionalAvailability(
-                        date = "2023-11-09".toLocalDate(),
-                        from = "08:00".toLocalTime(),
-                        to = "09:00".toLocalTime(),
-                    ),
-                )
-            )
-            delay(3000)
-            println("updating date")
-            d.value = LocalDate(2024, 11, 2)
-        }
-        sut.getAvailabilityData(pro, d)
-            .collect {
-                println("Got a new data $it")
-            }
-    }
-
-    private fun professional(professionalAvailabilities: List<ProfessionalAvailability>) =
-        Professional(
-            1,
-            "test@email.com",
-            "Luba",
-            "Mikaela",
-            "Life coach",
-            100,
-            "EUR",
-            "https://i.imgur.com/5Yma8Kl.jpeg",
-            "5.0",
-            "former professional boxer who competed from 1985 to 2005",
-            availability = professionalAvailabilities,
-            reviewSize = "100",
-        )
 }
