200852db31847002b1ec1307bd36bd7e868c1dce
hulkdx <hulktroll@gmail.com>
2024-01-02 15:06:36 +0200

    Added useCase

---
+++ common/src/commonMain/kotlin/com/hulkdx/findprofessional/common/feature/review/ReviewModule.kt
@@ -0,0 +1,14 @@
+package com.hulkdx.findprofessional.common.feature.home
+
+import com.hulkdx.findprofessional.common.feature.home.detail.availability.HomeDetailAvailabilityUseCase
+import org.koin.core.module.Module
+import org.koin.core.module.dsl.factoryOf
+import org.koin.dsl.bind
+import org.koin.dsl.module
+
+val homeModule: Module
+    get() = module {
+        factoryOf(::ProfessionalApiImpl) bind ProfessionalApi::class
+        factoryOf(::HomeUseCase)
+        factoryOf(::HomeDetailAvailabilityUseCase)
+    }
