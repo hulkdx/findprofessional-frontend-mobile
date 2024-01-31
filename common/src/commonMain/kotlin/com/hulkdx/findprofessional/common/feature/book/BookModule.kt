57728ef75ba3715f17392d6a51ca293aaac00be9
hulkdx <hulktroll@gmail.com>
2024-01-31 16:24:35 +0200

    Fixed a crash

--- common/src/commonMain/kotlin/com/hulkdx/findprofessional/common/feature/book/BookModule.kt
+++ common/src/commonMain/kotlin/com/hulkdx/findprofessional/common/feature/book/BookModule.kt
@@ -1,10 +1,11 @@
 package com.hulkdx.findprofessional.common.feature.book
 
 import org.koin.core.module.Module
-import org.koin.core.module.dsl.factoryOf
 import org.koin.dsl.module
 
 val bookModule: Module
     get() = module {
-        factoryOf(::BookUseCase)
+        factory {
+            BookUseCase()
+        }
     }
