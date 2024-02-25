c56408af63cc760bbfe4b163b066777f56b7f91a
hulkdx <hulktroll@gmail.com>
2024-02-25 18:22:57 +0200

    Profile improvements

--- android/feature/profile/src/main/kotlin/com/hulkdx/findprofessional/feature/profile/ProfileScreen.kt
+++ android/feature/profile/src/main/kotlin/com/hulkdx/findprofessional/feature/profile/ProfileScreen.kt
@@ -3,14 +3,14 @@ package com.hulkdx.findprofessional.feature.profile
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.systemBarsPadding
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.KeyboardArrowRight
 import androidx.compose.material3.Icon
@@ -22,14 +22,15 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import com.hulkdx.findprofessional.core.commonui.CUSnackBar
 import com.hulkdx.findprofessional.core.theme.AppTheme
 import com.hulkdx.findprofessional.core.theme.body1
-import com.hulkdx.findprofessional.feature.navigation.navbar.AppNavigationBar
+import com.hulkdx.findprofessional.core.theme.h1Medium
+import com.hulkdx.findprofessional.feature.navigation.navbar.AppNavBarContainer
+import com.hulkdx.findprofessional.resources.MR
 import dev.icerock.moko.resources.compose.localized
 import org.koin.androidx.compose.koinViewModel
 
@@ -51,26 +52,39 @@ fun ProfileScreen(
     error: String?,
     onErrorDismissed: () -> Unit,
 ) {
-    Box(
-        modifier = Modifier
-            .background(MaterialTheme.colorScheme.onTertiary)
-            .fillMaxSize()
-            .systemBarsPadding()
-            .testTag("ProfileScreen")
+    AppNavBarContainer(
+        testTag = "ProfileScreen",
+        error = error,
+        onErrorDismissed = onErrorDismissed,
     ) {
-        Column {
-            ProfileItem(
-                text = "Logout",
-                icon = Icons.Filled.KeyboardArrowRight,
-                onClick = onLogoutClicked,
-            )
-        }
-        CUSnackBar(
-            modifier = Modifier.align(Alignment.BottomCenter),
-            message = error,
-            onDismiss = onErrorDismissed
+        ProfileScreen(
+            onLogoutClicked = onLogoutClicked,
         )
-        AppNavigationBar()
+    }
+}
+
+@Composable
+fun ProfileScreen(
+    onLogoutClicked: () -> Unit,
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .verticalScroll(rememberScrollState())
+    ) {
+        Text(
+            modifier = Modifier
+                .align(Alignment.CenterHorizontally)
+                .padding(top = 70.dp, bottom = 32.dp),
+            style = h1Medium,
+            text = stringResource(MR.strings.profile.resourceId),
+        )
+
+        ProfileItem(
+            text = "Logout",
+            icon = Icons.Filled.KeyboardArrowRight,
+            onClick = onLogoutClicked,
+        )
     }
 }
 
@@ -84,9 +98,10 @@ private fun ProfileItem(
         horizontalArrangement = Arrangement.SpaceBetween,
         modifier = Modifier
             .fillMaxWidth()
-            .padding(16.dp)
+            .padding(horizontal = 16.dp)
+            .padding(bottom = 8.dp)
             .clip(RoundedCornerShape(8.dp))
-            .background(MaterialTheme.colorScheme.onPrimary)
+            .background(MaterialTheme.colorScheme.surfaceVariant)
             .clickable(onClick = onClick)
             .padding(16.dp),
     ) {
