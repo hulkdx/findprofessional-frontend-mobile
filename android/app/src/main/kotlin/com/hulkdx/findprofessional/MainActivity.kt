271d6c78406198d9a42d00e7f9bd8353bc59efa0
hulkdx <hulktroll@gmail.com>
2024-06-16 19:43:46 +0300

    xcode 15.4 testing

--- android/app/src/main/kotlin/com/hulkdx/findprofessional/MainActivity.kt
+++ android/app/src/main/kotlin/com/hulkdx/findprofessional/MainActivity.kt
@@ -1,24 +1,19 @@
 package com.hulkdx.findprofessional
 
-import android.graphics.Color
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import com.hulkdx.findprofessional.core.theme.AppTheme
-import com.hulkdx.findprofessional.navigation.NavigationComposable
-import androidx.activity.enableEdgeToEdge
+import com.hulkdx.findprofessional.common.App
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        enableEdgeToEdge()
-        window.navigationBarColor = Color.BLACK
+//        enableEdgeToEdge()
+//        window.navigationBarColor = Color.BLACK
 
         setContent {
-            AppTheme {
-                NavigationComposable()
-            }
+            App()
         }
     }
 }
