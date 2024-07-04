dfab21cef22b1472f756013750c2b84a927cd27c
hulkdx <hulktroll@gmail.com>
2024-07-04 22:35:37 +0300

    small change

--- core/src/commonMain/kotlin/com/hulkdx/findprofessional/core/utils/SingleClick.kt
+++ core/src/commonMain/kotlin/com/hulkdx/findprofessional/core/utils/SingleClick.kt
@@ -1,9 +1,11 @@
 package com.hulkdx.findprofessional.core.utils
 
+import kotlinx.datetime.Clock
+
 fun singleClick(onClick: () -> Unit): () -> Unit {
     var latest: Long = 0
     return {
-        val now = System.currentTimeMillis()
+        val now = Clock.System.now().toEpochMilliseconds()
         if (now - latest >= 3000) {
             onClick()
             latest = now
