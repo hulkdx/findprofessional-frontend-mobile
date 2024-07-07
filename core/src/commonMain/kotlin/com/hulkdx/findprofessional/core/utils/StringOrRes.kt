5a153eb68b8fe911a38de34e1e5c132755dd35a2
hulkdx <hulktroll@gmail.com>
2024-07-07 18:20:31 +0300

    Small change

---
+++ core/src/commonMain/kotlin/com/hulkdx/findprofessional/core/utils/StringOrRes.kt
@@ -0,0 +1,18 @@
+package com.hulkdx.findprofessional.core.utils
+
+import androidx.compose.runtime.Composable
+import org.jetbrains.compose.resources.StringResource
+import org.jetbrains.compose.resources.stringResource
+
+data class StringOrRes(
+    val string: String? = null,
+    val res: StringResource? = null,
+) {
+    @Composable
+    inline fun localized(): String {
+        return string ?: stringResource(res!!)
+    }
+}
+
+fun String.toStringOrRes() = StringOrRes(string = this)
+fun StringResource.toStringOrRes() = StringOrRes(res = this)
