081ccd954f464c9093b2ee9addd9fee4a529f20e
hulkdx <hulktroll@gmail.com>
2024-07-07 18:29:33 +0300

    LoginApi added

---
+++ feature/auth/src/commonMain/kotlin/com/hulkdx/findprofessional/feature/authentication/login/LoginApi.kt
@@ -0,0 +1,32 @@
+package com.hulkdx.findprofessional.feature.authentication.login
+
+import com.hulkdx.findprofessional.feature.authentication.login.model.Auth
+import com.hulkdx.findprofessional.feature.authentication.login.model.LoginRequest
+import io.ktor.client.HttpClient
+import io.ktor.client.call.body
+import io.ktor.client.request.post
+import io.ktor.client.request.setBody
+import io.ktor.client.request.url
+import io.ktor.http.ContentType
+import io.ktor.http.contentType
+
+interface LoginApi {
+    suspend fun login(request: LoginRequest): Auth
+}
+
+class LoginApiImpl(
+    private val client: HttpClient,
+) : LoginApi {
+    override suspend fun login(request: LoginRequest): Auth {
+        return client.post {
+            url(urlString)
+            contentType(ContentType.Application.Json)
+            setBody(request)
+        }
+            .body()
+    }
+
+    companion object {
+        const val urlString = "auth/login"
+    }
+}
