f8d2a95ade682e6f7ad5e97228d6c02219633f78
hulkdx <hulktroll@gmail.com>
2024-07-10 17:41:01 +0300

    Fixed savedinstanceholder on android

--- core/src/commonMain/kotlin/org/koin/androidx/compose/KoinUtils.kt
+++ core/src/commonMain/kotlin/org/koin/androidx/compose/KoinUtils.kt
@@ -1,14 +1,37 @@
 package org.koin.androidx.compose
 
 import androidx.compose.runtime.Composable
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.createSavedStateHandle
+import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.compose.viewModel
 import org.koin.compose.currentKoinScope
+import org.koin.core.parameter.ParametersHolder
+import kotlin.reflect.KClass
 
 
 // TODO: replace it when koin has koinViewModel to compose multiplatform
 @Composable
-inline fun <reified VM : ViewModel> koinViewModel(): VM {
+inline fun <reified T : ViewModel> koinViewModel(): T {
     val scope = currentKoinScope()
-    return viewModel { scope.get() }
+    return viewModel { scope.get<T> { SavedInstanceParametersHolder(this) } }
+}
+
+class SavedInstanceParametersHolder(private val extras: CreationExtras) :
+    ParametersHolder(mutableListOf()) {
+
+    override fun <T> elementAt(i: Int, clazz: KClass<*>): T {
+        return createSavedStateHandleOrElse(clazz) { super.elementAt(i, clazz) }
+    }
+
+    override fun <T> getOrNull(clazz: KClass<*>): T? {
+        return createSavedStateHandleOrElse(clazz) { super.getOrNull(clazz) }
+    }
+
+    private fun <T> createSavedStateHandleOrElse(clazz: KClass<*>, block: () -> T): T {
+        return if (clazz == SavedStateHandle::class) {
+            extras.createSavedStateHandle() as T
+        } else block()
+    }
 }
