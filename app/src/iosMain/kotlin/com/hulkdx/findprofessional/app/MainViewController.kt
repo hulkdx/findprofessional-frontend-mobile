d49bd8f8713a08509c882189ef71f62079c94e25
hulkdx <hulktroll@gmail.com>
2024-07-11 18:16:00 +0300

    Fixed iOS-decompose

--- app/src/iosMain/kotlin/com/hulkdx/findprofessional/app/MainViewController.kt
+++ app/src/iosMain/kotlin/com/hulkdx/findprofessional/app/MainViewController.kt
@@ -1,6 +1,17 @@
 package com.hulkdx.findprofessional.app
 
 import androidx.compose.ui.window.ComposeUIViewController
+import com.arkivanov.decompose.DefaultComponentContext
+import com.arkivanov.essenty.lifecycle.ApplicationLifecycle
+import com.hulkdx.findprofessional.libs.navigation.decompose.ComponentContext
+import platform.UIKit.UIViewController
 
 @Suppress("unused", "FunctionName")
-fun MainViewController() = ComposeUIViewController { App() }
+fun MainViewController(): UIViewController {
+    // If this caused issue, use context like this:
+    // https://arkivanov.github.io/Decompose/getting-started/quick-start/#ios-with-compose-or-swiftui-without-the-experimental-applicationlifecycle
+    val context = ComponentContext(DefaultComponentContext(ApplicationLifecycle()))
+    return ComposeUIViewController {
+        App(context)
+    }
+}
