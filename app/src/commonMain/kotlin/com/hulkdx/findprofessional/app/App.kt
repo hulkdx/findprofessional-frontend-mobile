18eabc18479c9d6f43087c6223ce271ac6d30086
hulkdx <hulktroll@gmail.com>
2024-07-11 20:15:12 +0300

    LoginScreen added

--- app/src/commonMain/kotlin/com/hulkdx/findprofessional/app/App.kt
+++ app/src/commonMain/kotlin/com/hulkdx/findprofessional/app/App.kt
@@ -1,22 +1,25 @@
 package com.hulkdx.findprofessional.app
 
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import com.hulkdx.findprofessional.core.navigation.NavigationScreen
+import com.hulkdx.findprofessional.core.theme.AppTheme
+import com.hulkdx.findprofessional.feature.authentication.login.LoginScreen
 import com.hulkdx.findprofessional.feature.authentication.splash.SplashScreen
 import com.hulkdx.findprofessional.libs.navigation.decompose.ComponentContext
 import com.hulkdx.findprofessional.libs.navigation.decompose.RootContent
 
 @Composable
 fun App(componentContext: ComponentContext) {
-    RootContent(componentContext) { RenderScreen(it) }
+    AppTheme {
+        RootContent(componentContext) { RenderScreen(it) }
+    }
 }
 
 @Composable
 private fun RenderScreen(screen: NavigationScreen) {
     when (screen) {
         NavigationScreen.Splash -> SplashScreen()
-        NavigationScreen.Login -> Text("login")
+        NavigationScreen.Login -> LoginScreen()
         else -> TODO()
     }
 }
