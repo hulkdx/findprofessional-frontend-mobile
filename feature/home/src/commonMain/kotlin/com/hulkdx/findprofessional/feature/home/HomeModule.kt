b3aeff74c5f5e16e4aa612fdc285c006d0ea0e71
hulkdx <hulktroll@gmail.com>
2024-07-20 00:24:24 +0300

    home viewmodel added

--- feature/home/src/commonMain/kotlin/com/hulkdx/findprofessional/feature/home/HomeModule.kt
+++ feature/home/src/commonMain/kotlin/com/hulkdx/findprofessional/feature/home/HomeModule.kt
@@ -1,11 +1,16 @@
 package com.hulkdx.findprofessional.feature.home
 
+import com.hulkdx.findprofessional.feature.home.api.ProfessionalApi
+import com.hulkdx.findprofessional.feature.home.api.ProfessionalApiImpl
 import org.koin.core.module.Module
 import org.koin.core.module.dsl.factoryOf
+import org.koin.dsl.bind
 import org.koin.dsl.module
 
 
 val homeModule: Module
     get() = module {
         factoryOf(::HomeViewModel)
+        factoryOf(::ProfessionalUseCase)
+        factoryOf(::ProfessionalApiImpl) bind ProfessionalApi::class
     }
