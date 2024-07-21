7328dd9aff1a61930c520712c233def60cdb3370
hulkdx <hulktroll@gmail.com>
2024-07-21 23:58:26 +0300

    BookingSummery

---
+++ feature/book/src/commonMain/kotlin/com/hulkdx/findprofessional/feature/book/summery/BookingSummeryUseCase.kt
@@ -0,0 +1,25 @@
+package com.hulkdx.findprofessional.feature.book.summery
+
+import com.hulkdx.findprofessional.core.model.book.SelectedTimes
+import com.hulkdx.findprofessional.core.model.pro.Professional
+import com.hulkdx.findprofessional.feature.authentication.login.storage.datastore.UserStorageDataStore
+import com.hulkdx.findprofessional.feature.book.summery.model.BookingSummeryUiState
+
+class BookingSummeryUseCase(
+    private val bookingSummeryTimeMapper: BookingSummeryTimeMapper,
+    private val userStore: UserStorageDataStore,
+) {
+    suspend fun getUiState(
+        professional: Professional,
+        times: SelectedTimes,
+    ): BookingSummeryUiState {
+        val uiTimes = bookingSummeryTimeMapper.map(times)
+        val user = userStore.get()?.user
+
+        return BookingSummeryUiState(
+            times = uiTimes,
+            userSkypeId = user?.skypeId,
+            totalPrices = bookingSummeryTimeMapper.calculateTotalPrices(professional, uiTimes.size)
+        )
+    }
+}
