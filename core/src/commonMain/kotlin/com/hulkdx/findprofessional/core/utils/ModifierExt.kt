d45c959c63dd8997784c64d8b3df30550961a92b
hulkdx <hulktroll@gmail.com>
2024-07-21 14:57:09 +0300

    Small change

--- core/src/commonMain/kotlin/com/hulkdx/findprofessional/core/utils/ModifierExt.kt
+++ core/src/commonMain/kotlin/com/hulkdx/findprofessional/core/utils/ModifierExt.kt
@@ -2,17 +2,17 @@ package com.hulkdx.findprofessional.core.utils
 
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.Dp
 
 
 fun Modifier.borderTop(width: Dp, color: Color) = drawBehind {
     val strokeWidth = width.toPx()
-    val y = strokeWidth / 2
     drawLine(
         color = color,
-        start = androidx.compose.ui.geometry.Offset(0f, y),
-        end = androidx.compose.ui.geometry.Offset(size.width, y),
+        start = Offset(0f, 0f),
+        end = Offset(size.width, 0f),
         strokeWidth = strokeWidth
     )
 }
