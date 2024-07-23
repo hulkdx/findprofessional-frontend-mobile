bb541b506a403edf9a376c576ace891a282332de
hulkdx <hulktroll@gmail.com>
2024-07-23 21:22:27 +0300

    Cloned Review composable functions

--- feature/review/src/commonMain/kotlin/com/hulkdx/findprofessional/feature/review/ReviewScreen.kt
+++ feature/review/src/commonMain/kotlin/com/hulkdx/findprofessional/feature/review/ReviewScreen.kt
@@ -1,35 +1,48 @@
 package com.hulkdx.findprofessional.feature.review
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.CenterVertically
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import com.hulkdx.findprofessional.core.commonui.CUAsyncImage
 import com.hulkdx.findprofessional.core.commonui.CUSnackBar
 import com.hulkdx.findprofessional.core.commonui.pagination.SetupOnLastItemVisible
 import com.hulkdx.findprofessional.core.model.pro.Professional
 import com.hulkdx.findprofessional.core.model.pro.ProfessionalReview
 import com.hulkdx.findprofessional.core.model.user.User
 import com.hulkdx.findprofessional.core.resources.Res
+import com.hulkdx.findprofessional.core.resources.ic_star2
 import com.hulkdx.findprofessional.core.resources.reviews
 import com.hulkdx.findprofessional.core.theme.AppTheme
+import com.hulkdx.findprofessional.core.theme.body1
+import com.hulkdx.findprofessional.core.theme.body1Medium
+import com.hulkdx.findprofessional.core.theme.body3
 import com.hulkdx.findprofessional.core.theme.h1Medium
-import com.hulkdx.findprofessional.feature.home.detail.review.ReviewContent
 import kotlinx.datetime.Clock
+import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.resources.stringResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
 import org.koin.androidx.compose.koinViewModel
@@ -70,7 +83,7 @@ fun ReviewScreen(
         val state = rememberLazyListState()
         SetupOnLastItemVisible(state, onLastItemVisible)
         LazyColumn(state = state) {
-            item { Header(reviewSize) }
+            item { ReviewHeader(reviewSize) }
             items(reviews, key = { it.id }) { ReviewContent(it) }
         }
         CUSnackBar(
@@ -82,7 +95,7 @@ fun ReviewScreen(
 }
 
 @Composable
-fun Header(reviewSize: String) {
+fun ReviewHeader(reviewSize: String) {
     Text(
         modifier = Modifier
             .fillMaxWidth()
@@ -93,6 +106,98 @@ fun Header(reviewSize: String) {
         style = h1Medium,
         textAlign = TextAlign.Center,
         text = "$reviewSize ${stringResource(Res.string.reviews)}",
+    )
+}
+
+@Composable
+fun ReviewContent(reviewContent: ProfessionalReview) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 16.dp)
+            .padding(bottom = 8.dp)
+            .clip(shape = RoundedCornerShape(10.dp))
+            .background(MaterialTheme.colorScheme.surfaceVariant)
+            .padding(bottom = 16.dp)
+    ) {
+        Header(reviewContent)
+        ReviewText(reviewContent.contentText)
+        ReviewDate(reviewContent.formattedDate)
+    }
+}
+
+
+@Composable
+private fun Header(review: ProfessionalReview) {
+    Row(Modifier.padding(top = 18.dp, start = 18.dp)) {
+        ReviewProfile(review.user.profileImage)
+        Column(
+            Modifier
+                .align(CenterVertically)
+                .padding(start = 12.dp)
+        ) {
+            ReviewName(review.user.fullName)
+            ReviewStar(review.rate)
+        }
+    }
+}
+
+
+@Composable
+private fun ReviewProfile(profileImageUrl: String?) {
+    CUAsyncImage(
+        modifier = Modifier
+            .size(50.dp)
+            .clip(shape = CircleShape), url = profileImageUrl ?: ""
+    )
+}
+
+@Composable
+private fun ReviewName(fullName: String) {
+    Text(
+        modifier = Modifier,
+        text = fullName,
+        color = MaterialTheme.colorScheme.outline,
+        style = body1Medium,
+    )
+}
+
+@Composable
+private fun ReviewStar(star: Int) {
+    Row(Modifier.padding(top = 2.dp)) {
+        for (i in 1..star) {
+            Image(
+                modifier = Modifier,
+                painter = painterResource(Res.drawable.ic_star2),
+                contentDescription = "",
+            )
+        }
+    }
+}
+
+
+@Composable
+private fun ReviewText(text: String?) {
+    text ?: return
+    Text(
+        modifier = Modifier
+            .padding(top = 12.dp)
+            .padding(horizontal = 16.dp),
+        text = text,
+        style = body1,
+        color = MaterialTheme.colorScheme.outline,
+    )
+}
+
+@Composable
+private fun ReviewDate(date: String) {
+    Text(
+        modifier = Modifier
+            .padding(top = 18.dp)
+            .padding(horizontal = 16.dp),
+        text = date,
+        style = body3,
+        color = MaterialTheme.colorScheme.onSurfaceVariant,
     )
 }
 
