05e2e25a4d3f4e229a56cee16ca4b6cae07d2201
hulkdx <hulktroll@gmail.com>
2024-11-03 17:44:09 +0200

    Temporary save

--- feature/pro/home/src/commonMain/kotlin/com/hulkdx/findprofessional/feature/pro/home/ProHomeScreen.kt
+++ feature/pro/home/src/commonMain/kotlin/com/hulkdx/findprofessional/feature/pro/home/ProHomeScreen.kt
@@ -1,11 +1,8 @@
 package com.hulkdx.findprofessional.feature.pro.home
 
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.testTag
 import com.hulkdx.findprofessional.feature.pro.home.tmp.EditAvailableTimeScreen
 import org.koin.androidx.compose.koinViewModel
 
--- feature/pro/home/src/commonMain/kotlin/com/hulkdx/findprofessional/feature/pro/home/tmp/EditAvailableTimeScreen.kt
+++ feature/pro/home/src/commonMain/kotlin/com/hulkdx/findprofessional/feature/pro/home/tmp/EditAvailableTimeScreen.kt
@@ -2,9 +2,9 @@ package com.hulkdx.findprofessional.feature.pro.home.tmp
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.material.icons.Icons
@@ -15,12 +15,14 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Alignment.Companion.CenterVertically
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import com.hulkdx.findprofessional.core.commonui.CUTextButton
 import com.hulkdx.findprofessional.core.commonui.CuDropDownMenu
 import com.hulkdx.findprofessional.core.resources.Res
+import com.hulkdx.findprofessional.core.resources.addNewTime
 import com.hulkdx.findprofessional.core.resources.editAvailableTime
 import com.hulkdx.findprofessional.core.theme.h2
 import com.hulkdx.findprofessional.core.theme.h3Medium
@@ -32,6 +34,7 @@ fun EditAvailableTimeScreen() {
     // TODO: move to viewmodel
     val availableTime = remember { (0..<24 * 60 step 30).map { TimeUtils.formattedTime(it) } }
 
+    // TODO: use LazyColumn
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -43,15 +46,19 @@ fun EditAvailableTimeScreen() {
             availableTime = availableTime,
             onDeleteClicked = {}
         )
+        AddNewTimeButton(onCLick = {})
+        ApplyButton()
+        ApplyToAllButton()
     }
 }
 
 @Composable
-private fun ColumnScope.Title() {
+private fun Title() {
     Text(
         modifier = Modifier
-            .padding(top = 32.dp, bottom = 16.dp)
-            .align(CenterHorizontally),
+            .padding(vertical = 32.dp)
+            .fillMaxWidth(),
+        textAlign = TextAlign.Center,
         text = stringResource(Res.string.editAvailableTime),
         style = h2,
     )
@@ -85,4 +92,35 @@ private fun TimeSlot(
             )
         }
     }
+}
+
+@Composable
+private fun AddNewTimeButton(onCLick: () -> Unit) {
+    CUTextButton(
+        modifier = Modifier.padding(horizontal = 24.dp),
+        text = stringResource(Res.string.addNewTime),
+        onClick = {},
+    )
+}
+
+@Composable
+private fun ApplyButton() {
+    CUTextButton(
+        modifier = Modifier.padding(horizontal = 24.dp),
+        text =  "Apply",
+// TODO:
+//        text = stringResource(Res.string.apply),
+        onClick = {},
+    )
+}
+
+@Composable
+private fun ApplyToAllButton() {
+    CUTextButton(
+        modifier = Modifier.padding(horizontal = 24.dp),
+        text =  "Apply to all",
+// TODO:
+//        text = stringResource(Res.string.apply),
+        onClick = {},
+    )
 }
