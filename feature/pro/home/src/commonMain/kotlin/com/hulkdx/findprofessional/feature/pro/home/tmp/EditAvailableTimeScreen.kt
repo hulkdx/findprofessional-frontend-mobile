dd30375d33b71c7c1bd2fce1085b4be42e725344
hulkdx <hulktroll@gmail.com>
2024-11-03 21:07:50 +0200

    Small change

--- feature/pro/home/src/commonMain/kotlin/com/hulkdx/findprofessional/feature/pro/home/tmp/EditAvailableTimeScreen.kt
+++ feature/pro/home/src/commonMain/kotlin/com/hulkdx/findprofessional/feature/pro/home/tmp/EditAvailableTimeScreen.kt
@@ -17,23 +17,54 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment.Companion.CenterVertically
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.capitalize
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import com.hulkdx.findprofessional.core.commonui.CUTextButton
 import com.hulkdx.findprofessional.core.commonui.CuDropDownMenu
 import com.hulkdx.findprofessional.core.resources.Res
 import com.hulkdx.findprofessional.core.resources.addNewTime
+import com.hulkdx.findprofessional.core.resources.applyToAll
+import com.hulkdx.findprofessional.core.resources.applyToOnly
 import com.hulkdx.findprofessional.core.resources.editAvailableTime
 import com.hulkdx.findprofessional.core.theme.h2
 import com.hulkdx.findprofessional.core.theme.h3Medium
 import com.hulkdx.findprofessional.core.utils.TimeUtils
+import kotlinx.datetime.LocalDate
 import org.jetbrains.compose.resources.stringResource
 
 @Composable
 fun EditAvailableTimeScreen() {
+    // TODO: get it from previous screen
+    val selectedDate = LocalDate(2024, 11, 3)
+
     // TODO: move to viewmodel
     val availableTime = remember { (0..<24 * 60 step 30).map { TimeUtils.formattedTime(it) } }
+    val dayOfWeek = selectedDate.dayOfWeek.name
+        .lowercase()
+        .replaceFirstChar { if (it.isLowerCase()) it.titlecase() else it.toString() }
+    val applyAllButtonText = stringResource(Res.string.applyToAll, dayOfWeek)
 
+    val month = selectedDate.month.name
+        .lowercase()
+        .replaceFirstChar { if (it.isLowerCase()) it.titlecase() else it.toString() }
+
+    val day = selectedDate.dayOfMonth
+    val applyButtonText = stringResource(Res.string.applyToOnly, "$month $day")
+
+    EditAvailableTimeScreen(
+        availableTime = availableTime,
+        applyButtonText = applyButtonText,
+        applyAllButtonText = applyAllButtonText,
+    )
+}
+
+@Composable
+private fun EditAvailableTimeScreen(
+    availableTime: List<String>,
+    applyButtonText: String,
+    applyAllButtonText: String,
+) {
     // TODO: use LazyColumn
     Column(
         modifier = Modifier
@@ -47,8 +78,8 @@ fun EditAvailableTimeScreen() {
             onDeleteClicked = {}
         )
         AddNewTimeButton(onCLick = {})
-        ApplyButton()
-        ApplyToAllButton()
+        ApplyButton(applyButtonText, onCLick = {})
+        ApplyToAllButton(applyAllButtonText, onCLick = {})
     }
 }
 
@@ -104,23 +135,19 @@ private fun AddNewTimeButton(onCLick: () -> Unit) {
 }
 
 @Composable
-private fun ApplyButton() {
+private fun ApplyButton(applyButtonText: String, onCLick: () -> Unit) {
     CUTextButton(
         modifier = Modifier.padding(horizontal = 24.dp),
-        text =  "Apply",
-// TODO:
-//        text = stringResource(Res.string.apply),
-        onClick = {},
+        text =  applyButtonText,
+        onClick = onCLick,
     )
 }
 
 @Composable
-private fun ApplyToAllButton() {
+private fun ApplyToAllButton(applyAllButtonText: String, onCLick: () -> Unit) {
     CUTextButton(
         modifier = Modifier.padding(horizontal = 24.dp),
-        text =  "Apply to all",
-// TODO:
-//        text = stringResource(Res.string.apply),
-        onClick = {},
+        text =  applyAllButtonText,
+        onClick = onCLick,
     )
 }
