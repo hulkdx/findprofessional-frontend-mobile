a4dd5b938d6a76a13528616c3e358904909af0c6
hulkdx <hulktroll@gmail.com>
2024-11-05 20:47:11 +0200

    Small change

--- core/src/commonMain/kotlin/com/hulkdx/findprofessional/core/utils/NumberFormatter.kt
+++ core/src/commonMain/kotlin/com/hulkdx/findprofessional/core/utils/NumberFormatter.kt
@@ -6,7 +6,7 @@ object NumberFormatter {
     fun twoDigits(number: Int): String {
         // NOTE: will do the same as following
         //
-        // String.format("%0$2d", number)
+        // String.format("%02d", number)
         //
         // String.format is not exists in native at the moment
         return if (number < 10) "0$number" else number.toString()
