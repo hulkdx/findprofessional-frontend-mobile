1247fd6ff528f3c2bf52f7d901a28772e2c8de51
hulkdx <hulktroll@gmail.com>
2026-01-08 17:04:22 +0200

    StringOrResource with argument

--- core/src/commonMain/kotlin/com/hulkdx/findprofessional/core/utils/StringOrRes.kt
+++ core/src/commonMain/kotlin/com/hulkdx/findprofessional/core/utils/StringOrRes.kt
@@ -5,16 +5,52 @@ import com.hulkdx.findprofessional.core.utils.serializer.StringResourceSerialize
 import kotlinx.serialization.Serializable
 import org.jetbrains.compose.resources.StringResource
 import org.jetbrains.compose.resources.stringResource
+import kotlin.jvm.JvmInline
 
 @Serializable
 data class StringOrRes(
     val string: String? = null,
     @Serializable(with = StringResourceSerializer::class)
     val res: StringResource? = null,
+    val resArgs: List<FormatArg>? = null,
 ) {
     @Composable
     fun localized(): String {
         return string ?: stringResource(res!!)
+    }
+
+    sealed interface FormatArg {
+        @Serializable
+        @JvmInline
+        value class Str(val v: String) : FormatArg
+
+        @Serializable
+        @JvmInline
+        value class IntArg(val v: Int) : FormatArg
+
+        @Serializable
+        @JvmInline
+        value class LongArg(val v: Long) : FormatArg
+
+        @Serializable
+        @JvmInline
+        value class DoubleArg(val v: Double) : FormatArg
+
+        @Serializable
+        @JvmInline
+        value class BoolArg(val v: Boolean) : FormatArg
+    }
+
+    companion object {
+        fun createStringResource(
+            res: StringResource,
+            vararg args: FormatArg,
+        ): StringOrRes {
+            return StringOrRes(
+                res = res,
+                resArgs = args.toList(),
+            )
+        }
     }
 }
 
